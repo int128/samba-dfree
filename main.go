bddc44171941b10e98e0c6985bc790387367046f
int128 <int128@gmail.com>
2015-11-29 11:23:10 +0900

    Treat first argument as exported dir

--- main.go
+++ main.go
@@ -2,29 +2,31 @@ package main
 
 import "os"
 import "fmt"
-import "log"
 import "io/ioutil"
 import "path/filepath"
 import "syscall"
 
 func main() {
-	wd, err := os.Getwd()
+	exportdir := "."
+	if len(os.Args) > 1 {
+		exportdir = os.Args[1]
+	}
+
+	absexportdir, err := filepath.Abs(exportdir)
 	if err != nil {
 		panic(err)
 	}
 
-	files, err := ioutil.ReadDir(wd)
+	files, err := ioutil.ReadDir(absexportdir)
 	if err != nil {
 		panic(err)
 	}
 
 	total, available := uint64(0), uint64(0)
 	for _, file := range files {
-		path := filepath.Join(wd, file.Name())
+		path := filepath.Join(absexportdir, file.Name())
 		var stat syscall.Statfs_t
-		if err := syscall.Statfs(path, &stat); err != nil {
-			log.Printf("Warning: ignored path %s (%s)\n", path, err)
-		} else {
+		if err := syscall.Statfs(path, &stat); err == nil {
 			total += stat.Blocks * uint64(stat.Bsize) / 1024
 			available += stat.Bavail * uint64(stat.Bsize) / 1024
 		}
